7c8e30a8f2685d9afa8170199a09881073004980
akrecev <akrecev@gmail.com>
2023-10-24 17:06:37 +0300

    initial

---
+++ src/test/java/com/kretsev/htmltableconverter/HtmlTableConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kretsev.htmltableconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlTableConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
